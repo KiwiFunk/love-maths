bd86daf050db3b642bf30feb3d5094ce2b944757
Alex <alex_p97@live.com>
2025-01-29 15:28:04 +0000

    Nested Functions inside of DOMContent listener

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,45 +1,45 @@
-
-
-// Populate btn-text with date from data-type attribute
+//Wait for DOM Content to load before executing game JS
 document.addEventListener('DOMContentLoaded', function() {
+    
+    // Populate btn-text with date from data-type attribute
     const buttons = document.querySelectorAll('.btn');
     buttons.forEach(button => {
         const btnText = button.nextElementSibling;
         btnText.textContent = button.getAttribute('data-type');
     });
+
+    function runGame() {
+
+    }
+    
+    function checkAnswer() {
+    
+    }
+    
+    function calculateCorrectAnswer() {
+    }
+    
+    
+    
+    function incrementScore() {
+    }
+    
+    function incrementWrongAnswer() {
+    }
+    
+    
+    
+    function displayAdditionQuestion() {
+    }
+    
+    function displaySubtractQuestion() {
+    }
+    
+    function displayMultiplyQuestion() {
+    }
+    
+    function displayDivideQuestion() {
+    }
+
 });
 
-function runGame() {
-
-}
-
-function checkAnswer() {
-
-}
-
-function calculateCorrectAnswer() {
-}
-
-
-
-
-function incrementScore() {
-}
-
-function incrementWrongAnswer() {
-}
-
-
-
-
-function displayAdditionQuestion() {
-}
-
-function displaySubtractQuestion() {
-}
-
-function displayMultiplyQuestion() {
-}
-
-function displayDivideQuestion() {
-}
