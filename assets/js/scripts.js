81518c1e26ebb6fc15d469aa500b88ff834fa114
Alex <alex_p97@live.com>
2025-01-29 16:27:56 +0000

    Added random number generation function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,9 @@
+//Global Values
+
+let maxVal = 25;
+
 //Wait for DOM Content to load before executing game JS
 document.addEventListener('DOMContentLoaded', function() {
-    
     
     const buttons = document.querySelectorAll('.btn');
     for (let button of buttons) {
@@ -20,43 +23,47 @@ document.addEventListener('DOMContentLoaded', function() {
         button.nextElementSibling.textContent = button.getAttribute('data-type');
     }
 
-
-
-
-
-
-    function runGame() {
-
-    }
-    
-    function checkAnswer() {
-    
-    }
-    
-    function calculateCorrectAnswer() {
-    }
-    
-    
-    
-    function incrementScore() {
-    }
-    
-    function incrementWrongAnswer() {
-    }
-    
-    
-    
-    function displayAdditionQuestion() {
-    }
-    
-    function displaySubtractQuestion() {
-    }
-    
-    function displayMultiplyQuestion() {
-    }
-    
-    function displayDivideQuestion() {
-    }
-
 });
 
+
+//Main game loop
+function runGame() {
+
+    let num1 = getRandomValue(maxVal);
+    let num2 = getRandomValue(maxVal);
+
+}
+
+//Use argument to set max value. perhaps implement a difficulty setting that increases or decreases max
+function getRandomValue(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+}
+
+
+
+function incrementScore() {
+}
+
+function incrementWrongAnswer() {
+}
+
+
+
+function displayAdditionQuestion() {
+}
+
+function displaySubtractQuestion() {
+}
+
+function displayMultiplyQuestion() {
+}
+
+function displayDivideQuestion() {
+}
